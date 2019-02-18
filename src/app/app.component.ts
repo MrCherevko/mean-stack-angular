285bbbc2f68fc9117a46bf5e87b87b838e84f475
ducky <ivan@getin.co.il>
2019-02-18 09:43:29 +0200

    Created post logic with header, and building post service to manipulate the data around application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Post } from './posts/post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  storedPosts: Post[] = [];
+
+  onPostAdded(post) {
+    this.storedPosts.push(post);
+  }
+
+}
+
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,21 @@
+import { PostsService } from './../post.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  posts: Post[] = [];
+
+  constructor(private postService: PostsService){
+    this.posts = this.postService.getPosts();
+  }
+
+  ngOnInit() {
+  }
+
+}
